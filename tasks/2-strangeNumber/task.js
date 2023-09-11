2b59ba11b442c79950f46d30abf3c867a45cc2e3
ethereal element <vitalik.safonov@list.ru>
2023-09-11 16:20:46 +0300

    init: strangeNumber

---
+++ tasks/2-strangeNumber/task.js
@@ -0,0 +1,15 @@
+const isStrangeNumber = (num) => {
+
+    if (+num < 1) {
+      return false;
+    }
+  
+    let sum = 0;
+    for (let i = 1; i < num; i++) {
+      if (+num % i === 0) {
+        sum += i;
+      }
+    }
+  
+    return sum === num;
+  }
