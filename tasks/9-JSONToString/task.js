a0c0df8d53e1b20e1cf5c97686735ea36a7f345d
ethereal element <vitalik.safonov@list.ru>
2023-09-12 17:57:45 +0300

    init: json to string task

---
+++ tasks/9-JSONToString/task.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const JsonData = {
+    name: 'Van',
+    age: 30,
+    surname: 'DarkHolm'
+};
+
+
+const JsonToString  = (json) => {
+    // для удобства - преобразовывем строку в массив
+    let res = []
+    // проходим циклом по объекту
+    for (const [key,value] of Object.entries(json)) {
+        // если число не ставим строковый литерал
+        if (typeof value === "number") {
+            res.push(`"${key}": ${Number(value)},`)
+        } else {res.push(`"${key}": "${value}",`)}
+    }
+    return `{${res.join("").split("").slice(0,-1).join("")}}`
+}
+
+console.log(JsonToString(JsonData));
