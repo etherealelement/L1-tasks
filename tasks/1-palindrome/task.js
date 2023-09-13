f450a7f2ba25eb710a0bb06107324f90743a946b
ethereal element <vitalik.safonov@list.ru>
2023-09-13 16:12:25 +0300

    fix: task-1

--- tasks/1-palindrome/task.js
+++ tasks/1-palindrome/task.js
@@ -1,8 +1,15 @@
 "use strict";
 
+// инициализируем стрелочную функцию.
 const isPalindrome = (str) => {
-    // получаем цельную строку без пробелов и сравниваем  
-   return str === str.split("").filter(item => item !== " ").reverse().join("");
+   // изначально правильно тип аргумента функции
+   // преобразуем исходную строку в массив избавляем от пробелов и превращаем обратно в строку
+   // с этой же строкой проделываем тоже самое только разворачиваем методом reverse
+   // после чего проверяем на строгое равенство и выводим результат на консоль
+   return typeof str === "string" ? str.split("").filter(item => item !== " ").join("") === str.split("").filter(item => item !== " ").reverse().join("") : false;
 }
 
-isPalindrome("аргентина манит негра");
+console.log(isPalindrome("аргентина манит негра")) // true
+console.log(isPalindrome("негр")) // false
+console.log(isPalindrome("NIGGER")) // false
+console.log(isPalindrome("ШАЛАШ")) // true
