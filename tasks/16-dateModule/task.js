f69d37f49300df56ce72a8d9a25ac8bc6495e59a
ethereal element <vitalik.safonov@list.ru>
2023-09-14 14:21:10 +0300

    init: config gitignore

--- tasks/16-dateModule/task.js
+++ tasks/16-dateModule/task.js
@@ -1,2 +1,18 @@
-"use strict"
+"use strict";
 
+// Устанавливаем библиотеку Moment.js через npm
+(function(){
+   const moment = require("moment");
+
+   // Функция для форматирования даты в заданном формате
+   const dateFormat = (date, format) => {
+       return moment(date).format(format)
+   }
+
+   // Экспортируем функцию из модуля
+
+    module.exports ={
+       dateFormat,
+    }
+
+})();
